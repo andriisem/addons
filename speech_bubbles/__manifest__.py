bd3f0f207c6c52f811e21d838393f573f3f79b2f
Andrey Semko <semko.andrey.i@gmail.com>
2018-10-12 00:45:16 +0300

    added basic style for chatter

---
+++ speech_bubbles/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Pure CSS speech bubbles',
+    'summary': 'Pure CSS speech bubbles',
+    'description': 'Pure CSS speech bubbles',
+    'author': 'Andrii Semko (@andriisem)',
+    'website': 'https://github.com/andriisem',
+    'category': '',
+    'sequence': 900,
+    'version': '0.1',
+    'depends': [],
+    'data': [
+        'views/assets.xml'
+    ],
+    'application': False,
+}
