873a22e8d4dcbb85f3aa01757e8ce34e1899a5ff
Andrey Semko <semko.andrey.i@gmail.com>
2018-11-18 18:54:05 +0200

    Added module for recalculation amount by currency

---
+++ recalculation_by_currency/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Recalculation by Currency',
+    'version': '0.1',
+    'description': """
+    - recalculation by currency
+    """,
+    'author': 'Andrey Semko | @andriisem',
+    'depends': [
+        'sale',
+        'account',
+    ],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+}
