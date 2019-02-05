aab41941a68229a66c5b0349b6de7210ad6c4af0
Andrey Semko <semko.andrey.i@gmail.com>
2019-02-05 16:00:19 +0200

    init 12.0

--- speech_bubbles/models/res_company.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class Company(models.Model):
-    _inherit = 'res.company'
-
-    use_speech_bubbles = fields.Boolean(string='Use Speech Bubbles')
--- speech_bubbles/models/res_config.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-
-class ResConfig(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    use_speech_bubbles = fields.Boolean(string='Use Speech Bubbles', related='company_id.use_speech_bubbles')
