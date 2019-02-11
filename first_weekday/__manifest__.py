e1353af0c3083be93cdf3a99519e5dba5844f1c2
Andrey Semko <semko.andrey.i@gmail.com>
2019-02-11 18:18:07 +0200

    addded module for datepicker

---
+++ first_weekday/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Datepicker related by locale",
+    "version": "1.0",
+    "category": "Accounting",
+    "description": """
+    """,
+    "author": "Andrii Semko | @andriisem",
+    "depends": [],
+    "data": [
+        'views/assets.xml',
+    ],
+    "installable": True,
+}
