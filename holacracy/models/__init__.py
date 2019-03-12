6373abe6f276f26f19292bdd0a1d2f2029f8c475
Andrey Semko <semko.andrey.i@gmail.com>
2019-03-12 18:35:16 +0200

    remove

--- holacracy/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import circles_tention
-from . import circles_role
-from . import holacracy_circles
--- holacracy/models/circles_tention.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-
-class CirclesTention(models.Model):
-    _name = 'circles.tention'
-    _description = 'circles.tention'
-
-    task_id = fields.Many2one('project.task', string='Task')
