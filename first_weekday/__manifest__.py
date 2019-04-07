b00211dae1bb7e09505ea2733b82b5b45d4a3dba
Andrey Semko <semko.andrey.i@gmail.com>
2019-04-07 17:57:41 +0300

    added description

--- first_weekday/__manifest__.py
+++ first_weekday/__manifest__.py
@@ -4,8 +4,8 @@
     "category": "Accounting",
     "description": """
     Datepicker related by locale:
-    - first day of the week
-    - translation
+    * first day of the week
+    * translation
     """,
     "author": "Andrii Semko | @andriisem",
     "website": "https://www.upwork.com/o/profiles/users/_~013175f63de76dd835/",
