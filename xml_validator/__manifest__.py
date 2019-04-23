09906e6eb9f3440c940bea4096b66523219d8410
Andrey Semko <semko.andrey.i@gmail.com>
2019-04-23 16:12:16 +0300

    description

--- xml_validator/__manifest__.py
+++ xml_validator/__manifest__.py
@@ -5,7 +5,7 @@
     "author": 'Andrii Semko | @andriisem',
     "website": "https://www.upwork.com/o/profiles/users/_~013175f63de76dd835/",
     "description": """
-        XML Validator
+        # The validation check is performed against any XML schema 
     """,
     "depends": ['base'],
     "data": [
