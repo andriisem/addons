76e3189b97b4be4c1649c9a7751a457cdbb8a170
Andrey Semko <semko.andrey.i@gmail.com>
2019-04-23 12:51:42 +0300

    init xml validator

---
+++ xml_validator/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "XML Validator",
+    "version": "1.0",
+    "category": "Tools",
+    "author": 'Andrii Semko | @andriisem',
+    "website": "https://www.upwork.com/o/profiles/users/_~013175f63de76dd835/",
+    "description": """
+        XML Validator
+    """,
+    "depends": [],
+    "data": [],
+    "application": False,
+    "installable": True,
+}
