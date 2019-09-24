679c220928f012533e0bb8d17ddb945d6bcabe85
Andrey Semko <semko.andrey.i@gmail.com>
2019-09-24 17:57:02 +0300

    added module nbu

---
+++ nbu_rates/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "NBU rates parser",
+    'description': """
+Parse NBU rates and updated Odoo active currencies rates accordingly
+=====================================================================
+    """,
+    'category': 'Accounting',
+    "author": 'Andrii Semko | @andriisem',
+    "website": "https://www.upwork.com/o/profiles/users/_~013175f63de76dd835/",
+    'version': '1.0',
+    'depends': [
+        'base',
+        'account',
+    ],
+
+    'data': [
+        'data/ir_cron.xml',
+        'views/res_currency_view.xml'
+    ],
+}
